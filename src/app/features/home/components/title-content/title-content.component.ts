615dc978d08326d1257cf99c6869262823b11351
RyanEweSeng <ryantoh1001@gmail.com>
2023-05-03 17:18:22 -0500

    Implement title content section for home

---
+++ src/app/features/home/components/title-content/title-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title-content',
+  templateUrl: './title-content.component.html',
+  styleUrls: ['./title-content.component.scss']
+})
+export class TitleContentComponent {
+
+}
