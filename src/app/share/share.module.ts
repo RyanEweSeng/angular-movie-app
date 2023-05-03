25693fc82af551c69ed3b9f82b790cb4a5be4a26
RyanEweSeng <ryantoh1001@gmail.com>
2023-05-03 17:13:36 -0500

    Create share module

---
+++ src/app/share/share.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    AboutComponent,
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    NavbarComponent,
+    AboutComponent,
+  ]
+})
+export class ShareModule { }
