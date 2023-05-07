ce86bbc6ee89cec97d572bc80d819fc70d8aafdb
RyanEweSeng <ryantoh1001@gmail.com>
2023-05-07 00:34:43 -0500

    Create browse components, services, and interfaces

--- src/app/features/browse/browse.module.ts
+++ src/app/features/browse/browse.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { MovieItemComponent } from './components/movie-item/movie-item.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MovieListComponent,
+    MovieItemComponent
+  ],
   imports: [
     CommonModule
   ]
