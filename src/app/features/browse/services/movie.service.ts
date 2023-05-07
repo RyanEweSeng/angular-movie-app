38e3d4f64ae354ad4febf58d892a79686ba6d49f
RyanEweSeng <ryantoh1001@gmail.com>
2023-05-07 16:43:29 -0500

    Implement movie service to get movie data

--- src/app/features/browse/services/movie.service.ts
+++ src/app/features/browse/services/movie.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  private apiKey = 'f17868bf6f3a716de2511915793ad50c';
+  private apiUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=' + this.apiKey;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
 }
