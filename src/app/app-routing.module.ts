efac241a93086e8bf186549843982795c9ee8c07
RyanEweSeng <ryantoh1001@gmail.com>
2023-05-12 01:32:54 -0500

    Remove unused imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './features/home/home.component';
-import { LoginComponent } from './features/login/login.component';
-import { RegisterComponent } from './features/register/register.component';
-import { BrowseComponent } from './features/browse/browse.component';
 
 const routes: Routes = [
   {
