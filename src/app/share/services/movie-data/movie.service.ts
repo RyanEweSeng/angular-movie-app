9a6c0dcb55a07e46a61fa07ad31563db50967762
RyanEweSeng <ryantoh1001@gmail.com>
2023-05-12 02:09:07 -0500

    Add function to get movie details

--- src/app/share/services/movie-data/movie.service.ts
+++ src/app/share/services/movie-data/movie.service.ts
@@ -8,11 +8,14 @@ import { Observable } from 'rxjs';
 export class MovieService {
   private apiKey = 'f17868bf6f3a716de2511915793ad50c';
   private apiUrl = 'https://api.themoviedb.org/3';
-  private endpoint = '/discover/movie';
 
   constructor(private http: HttpClient) { }
 
   getData(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}${this.endpoint}?api_key=${this.apiKey}`);
+    return this.http.get<any>(`${this.apiUrl}/discover/movie?api_key=${this.apiKey}`);
+  }
+
+  getDetails(movieId: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/movie/${movieId}?api_key=${this.apiKey}`);
   }
 }
