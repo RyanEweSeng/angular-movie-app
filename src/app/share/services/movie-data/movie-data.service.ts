b1af77ade90dff6c2bf80d042a83f6063e8a9736
RyanEweSeng <ryantoh1001@gmail.com>
2023-05-12 02:04:01 -0500

    Change how api url is constructed

--- src/app/share/services/movie-data/movie-data.service.ts
+++ src/app/share/services/movie-data/movie-data.service.ts
@@ -7,11 +7,12 @@ import { Observable } from 'rxjs';
 })
 export class MovieService {
   private apiKey = 'f17868bf6f3a716de2511915793ad50c';
-  private apiUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=' + this.apiKey;
+  private apiUrl = 'https://api.themoviedb.org/3';
+  private endpoint = '/discover/movie';
 
   constructor(private http: HttpClient) { }
 
   getData(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
+    return this.http.get<any>(`${this.apiUrl}${this.endpoint}?api_key=${this.apiKey}`);
   }
 }
