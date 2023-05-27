73aff8a005d6a84e6edb397cf31a8e1dfa927d5c
RyanEweSeng <ryantoh1001@gmail.com>
2023-05-27 14:11:11 -0500

    Move resolvers directory to core module

--- src/app/features/browse/browse-routing.module.ts
+++ src/app/features/browse/browse-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { BrowseComponent } from './browse.component';
 import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
-import { MovieDetailResolver } from 'src/app/share/resolvers/movie-detail/movie-detail.resolver';
-import { TrailerResolver } from 'src/app/share/resolvers/trailer/trailer.resolver';
+import { MovieDetailResolver } from 'src/app/core/resolvers/movie-detail/movie-detail.resolver';
+import { TrailerResolver } from 'src/app/core/resolvers/trailer/trailer.resolver';
 
 const routes: Routes = [
   {
