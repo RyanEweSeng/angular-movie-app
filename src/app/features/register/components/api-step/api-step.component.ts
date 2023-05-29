fe4e271e08b17fe920c82d23db43bccab7265077
RyanEweSeng <ryantoh1001@gmail.com>
2023-05-29 13:56:02 -0500

    Use dropdown instead of radio buttons

--- src/app/features/register/components/api-step/api-step.component.ts
+++ src/app/features/register/components/api-step/api-step.component.ts
@@ -1,6 +1,14 @@
 import { Component, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormGroupDirective, NgForm } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 import { FloatLabelType } from '@angular/material/form-field';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-api-step',
@@ -10,6 +18,7 @@ import { FloatLabelType } from '@angular/material/form-field';
 export class ApiStepComponent {
   @Input() parentFormGroup!: FormGroup;
 
+  matcher: MyErrorStateMatcher = new MyErrorStateMatcher();
   readonly apiKeyMinLength: number = 15;
 
   constructor() { }
